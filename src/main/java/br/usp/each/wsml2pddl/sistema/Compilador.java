e81d6ed2e15ebe6a4ac266ef6e2b120060c86472
Felipe Pierin <felipe.pierin@gmail.com>
2011-09-26 12:15:17 -0300

    Realocacao de pacote de teste do sistema

--- src/main/java/br/usp/each/wsml2pddl/sistema/Compilador.java
+++ src/main/java/br/usp/each/wsml2pddl/sistema/Compilador.java
@@ -1,4 +1,4 @@
-package br.usp.each.wsml2pddl.gramaticas;
+package br.usp.each.wsml2pddl.sistema;
 
 import java.io.IOException;
 
@@ -10,6 +10,9 @@ import org.antlr.runtime.TokenStream;
 import org.antlr.runtime.tree.CommonTreeNodeStream;
 
 import br.usp.each.wsml2pddl.evaluators.Evaluator;
+import br.usp.each.wsml2pddl.gramaticas.ProblemTranslator;
+import br.usp.each.wsml2pddl.gramaticas.WsmlLexer;
+import br.usp.each.wsml2pddl.gramaticas.WsmlParser;
 
 public class Compilador {
 
@@ -20,7 +23,7 @@ public class Compilador {
 		final TokenStream tokenStream = new CommonTokenStream(lexer);
 		final WsmlParser wsmlParser = new WsmlParser(tokenStream);
 		
-		final CommonTreeNodeStream nodeStream = new CommonTreeNodeStream(wsmlParser.wsml().tree);
+		final CommonTreeNodeStream nodeStream = new CommonTreeNodeStream(wsmlParser.wsml().getTree());
 		final ProblemTranslator problemTranslator =  new ProblemTranslator(nodeStream);
 		final Evaluator evaluator = problemTranslator.evaluator();
 		System.out.println(evaluator.evaluate()); 
