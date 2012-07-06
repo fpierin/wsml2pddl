0e663cfe4ac625626cbc7ce4edbdec8557e859e9
Felipe <felipe@hawkYbr71.(none)>
2012-07-05 23:27:56 -0300

    correcao de pom e geracao de classes

--- src/main/java/br/usp/each/wsml2pddl/Exemplo.java
+++ src/main/java/br/usp/each/wsml2pddl/Exemplo.java
@@ -5,25 +5,24 @@ import java.io.File;
 import br.usp.each.wsml2pddl.sistema.Compilador;
 import br.usp.each.wsml2pddl.sistema.impl.CompiladorWsmlPDDL;
 
-
 /**
  * 
  * @author Felipe Pierin <felipe.pierin@yahoo.com.br>
- *
+ * 
  */
 public class Exemplo {
 
 	public static void main(final String[] args) {
 
 		final Compilador compilador = new CompiladorWsmlPDDL();
+		System.out.println("\n\nRealizando tradução de um goal WSML para um problema PDDL\n\n");
+		final File arquivo = new File("src/main/resources/goal.wsml");
+		final String traducao = compilador.compila(arquivo);
+		System.out.println(traducao);
 
-		// System.out.println("Realizando tradução de ontologia WSML para um dominio PDDL\n\n");
-		// System.out.println(compilador.compila(new
-		// File("src/main/java/ontologia.wsml")));
-
-		System.out
-				.println("\n\nRealizando tradução de um goal WSML para um problema PDDL\n\n");
-		System.out.println(compilador.compila(new File("classpath:goal.wsml")));
+		final File arquivo2 = new File("src/main/resources/ontologia.wsml");
+		final String traducao2 = compilador.compila(arquivo2);
+		System.out.println(traducao2);
 
 	}
 
